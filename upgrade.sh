9f13c2060b7a7857741822a4f6854acd0535d2ab
Nicolas Gaillard <nicolas.gaillard@etu.univ-nantes.fr>
2020-04-11 17:55:57 +0200

    upgrade script

---
+++ upgrade.sh
@@ -0,0 +1,20 @@
+# OSX / brew update script
+
+# - Brew -
+# upgrade
+brew update
+brew upgrade
+brew cleanup -s
+
+# now diagnotic
+brew doctor
+brew missing
+
+# - OSX -
+echo “you can hit mas upgrade to upgrade theses apps from the app store:”
+mas outdated
+echo “install with: mas upgrade”
+
+# - Others -
+# npm update -g
+echo “and pip ? pip freeze — local | grep -v ‘^\-e’ | cut -d = -f 1 | xargs pip install -U “
